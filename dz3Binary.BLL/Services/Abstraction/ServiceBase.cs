98ddf0e6376f25dad0663901e078dde286e9486e
unknown <kovalikn6@gmail.com>
2022-07-13 14:32:53 +0300

    added crud operations

--- dz3Binary.BLL/Services/Abstraction/ServiceBase.cs
+++ dz3Binary.BLL/Services/Abstraction/ServiceBase.cs
@@ -1,9 +1,10 @@
 ﻿using AutoMapper;
 using dz3Binary.DAL.Context;
-
+using dz3Binary.DAL.Entities;
+using System.Threading.Tasks;
 namespace dz3Binary.BLL.Services.Abstraction;
 
-public abstract class ServiceBase
+public abstract class ServiceBase<T> : IService<T> where T : BaseEntity
 {
     protected readonly ProjectContext _context;
     protected readonly IMapper _mapper;
@@ -11,5 +12,36 @@ public abstract class ServiceBase
     {
         _context = context;
         _mapper = mapper;
+    }
+
+    public IEnumerable<T> Get()
+    {
+        return _context.Set<T>();
+    }
+
+    public T GetById(int id)
+    {
+        return _context.Set<T>().First(x => x.Id == id);
+    }
+
+    public async System.Threading.Tasks.Task IService<T>.Add(T entity)
+    {
+        if(entity is null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        _context.Add(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public System.Threading.Tasks.Task IService<T>.Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    System.Threading.Tasks.Task IService<T>.Update(T entity)
+    {
+        throw new NotImplementedException();
     }
 }
